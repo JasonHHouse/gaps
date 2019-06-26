34bcb24e7bc8e25b29a0be8b1c46f79ec8d0d4f8
Jason House <jh5975@gmail.com>
2019-06-25 21:24:37 -0400

    Automatically adding movie libraries now to let users select

--- src/main/resources/static/js/plex_libraries.js
+++ src/main/resources/static/js/plex_libraries.js
@@ -29,8 +29,55 @@ function onStart() {
 
     //populateCookieValues();
 
+    getLibraries();
+
     M.AutoInit();
     M.updateTextFields();
+}
+
+function getLibraries() {
+
+    let obj = JSON.parse(document.cookie);
+    let token = obj.plex_token;
+    let port = obj.port;
+    let address = obj.address;
+
+    if (!token || !port || !address) {
+        console.error('Could not find plex token, port, or address in cookies');
+        //Return error toast here
+        return;
+    }
+
+    let data = {
+        token: token,
+        port: port,
+        address: address
+    };
+
+    $.ajax({
+        type: "GET",
+        url: "http://" + location.hostname + ":" + location.port + "/getPlexLibraries?" + encodeQueryData(data),
+        contentType: "application/json",
+        success: function (data) {
+            generateLibrariesCheckbox(data);
+        }
+    });
+}
+
+function encodeQueryData(data) {
+    const ret = [];
+    for (let d in data)
+        ret.push(encodeURIComponent(d) + '=' + encodeURIComponent(data[d]));
+    return ret.join('&');
+}
+
+function generateLibrariesCheckbox(movies) {
+    let row = '';
+    for (let movie of movies) {
+        row += '<div class="row"><p class="col s5"><label><input type="checkbox" class="filled-in" /><span>' + movie.title + '</span></label></p></div>';
+    }
+
+    $('#libraryCheckboxes').html(row);
 }
 
 function populateCookieValues() {
