ffe9eafc89f838b9fdcd5864dc2ed34059bda46b
jhouse <jhouse@indeed.com>
2019-12-20 12:00:58 +0900

    Updating RSS empty state

--- GapsWeb/src/main/java/com/jasonhhouse/gaps/controller/RSSController.java
+++ GapsWeb/src/main/java/com/jasonhhouse/gaps/controller/RSSController.java
@@ -1,12 +1,15 @@
 package com.jasonhhouse.gaps.controller;
 
 import com.jasonhhouse.gaps.service.IoService;
+
 import java.io.IOException;
+
 import org.apache.commons.lang3.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.ModelAndView;
 
 @RestController
 public class RSSController {
@@ -19,7 +22,7 @@ public class RSSController {
     }
 
     @GetMapping(path = "/rss")
-    public String rss(Model model) throws IOException {
+    public ModelAndView rss() throws IOException {
         String rss = null;
         if (ioService.doesRssFileExist()) {
             rss = ioService.getRssFile();
@@ -27,9 +30,11 @@ public class RSSController {
 
         if (StringUtils.isEmpty(rss)) {
             //Show empty page
-            return "emptyState";
+            return new ModelAndView("emptyState");
         } else {
-            return rss;
+            ModelAndView modelAndView = new ModelAndView("rss");
+            modelAndView.addObject("rss", rss);
+            return modelAndView;
         }
     }
 
