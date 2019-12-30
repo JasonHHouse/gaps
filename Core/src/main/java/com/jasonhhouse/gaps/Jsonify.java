4a38efca1061e6cc3e21b5df38d40583dcba83bd
Jason House <jh5975@gmail.com>
2019-12-30 16:39:21 +0900

    Cleaning up code

--- Core/src/main/java/com/jasonhhouse/gaps/Jsonify.java
+++
@@ -1,9 +0,0 @@
-package com.jasonhhouse.gaps;
-
-import com.fasterxml.jackson.databind.node.ObjectNode;
-
-public interface Jsonify<T> {
-
-    ObjectNode toJSON();
-
-}
--- Core/src/main/java/com/jasonhhouse/gaps/PlexLibrary.java
+++ Core/src/main/java/com/jasonhhouse/gaps/PlexLibrary.java
@@ -1,5 +1,5 @@
-package com.jasonhhouse.gaps;/*
- * Copyright 2019 Jason H House
+/*
+ * Copyright 2020 Jason H House
  *
  * Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
  *
@@ -7,6 +7,8 @@ package com.jasonhhouse.gaps;/*
  *
  * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
  */
+
+package com.jasonhhouse.gaps;
 
 import java.util.Objects;
 import org.jetbrains.annotations.NotNull;
@@ -54,12 +56,13 @@ public class PlexLibrary implements Comparable<PlexLibrary> {
         if (this == o) return true;
         if (o == null || getClass() != o.getClass()) return false;
         PlexLibrary that = (PlexLibrary) o;
-        return Objects.equals(key, that.key);
+        return Objects.equals(key, that.key) &&
+                Objects.equals(title, that.title);
     }
 
     @Override
     public int hashCode() {
-        return Objects.hash(key);
+        return Objects.hash(key, title);
     }
 
     @Override
