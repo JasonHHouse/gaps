d2d22478c6ed64c6bfec52f68cb51478732d229c
jhouse <jhouse@indeed.com>
2020-02-07 15:55:50 +0900

    Cleaning up code a bit

--- cypress/integration/configuration.plex.spec.js
+++ cypress/integration/configuration.plex.spec.js
@@ -2,16 +2,16 @@ let appHasStarted;
 
 function spyOnAddEventListener(win) {
     // win = window object in our application
-    const addListener = win.EventTarget.prototype.addEventListener
+    const addListener = win.EventTarget.prototype.addEventListener;
     win.EventTarget.prototype.addEventListener = function (name) {
         if (name === 'change') {
             // web app added an event listener to the input box -
             // that means the web application has started
-            appHasStarted = true
+            appHasStarted = true;
             // restore the original event listener
-            win.EventTarget.prototype.addEventListener = addListener
+            win.EventTarget.prototype.addEventListener = addListener;
         }
-        return addListener.apply(this, arguments)
+        return addListener.apply(this, arguments);
     }
 }
 
