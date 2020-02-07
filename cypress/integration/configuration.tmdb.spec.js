c110297fb4b8e13ac0afc28a07efa9b0855c4c11
jhouse <jhouse@indeed.com>
2020-02-07 09:31:59 +0900

    Adding cypress tests for TMDB id

---
+++ cypress/integration/configuration.tmdb.spec.js
@@ -0,0 +1,122 @@
+let appHasStarted;
+
+function spyOnAddEventListener(win) {
+    // win = window object in our application
+    const addListener = win.EventTarget.prototype.addEventListener
+    win.EventTarget.prototype.addEventListener = function (name) {
+        if (name === 'change') {
+            // web app added an event listener to the input box -
+            // that means the web application has started
+            appHasStarted = true
+            // restore the original event listener
+            win.EventTarget.prototype.addEventListener = addListener
+        }
+        return addListener.apply(this, arguments)
+    }
+}
+
+it('Enter invalid TMDB Key', () => {
+    cy.visit('/configuration', {onBeforeLoad: spyOnAddEventListener});
+
+    cy.get('#movieDbApiKey')
+        .clear()
+        .type('ABC123')
+        .should('have.value', 'ABC123');
+
+    cy.get('#testTmdbKey')
+        .click();
+
+    cy.get('#tmdbTestError')
+        .should('be.visible');
+
+    cy.get('#tmdbTestSuccess')
+        .should('not.be.visible');
+
+    cy.get('#tmdbSaveError')
+        .should('not.be.visible');
+
+    cy.get('#tmdbSaveSuccess')
+        .should('not.be.visible');
+});
+
+it('Enter valid TMDB Key', () => {
+    cy.visit('/configuration', {onBeforeLoad: spyOnAddEventListener});
+
+    cy.get('#movieDbApiKey')
+        .clear()
+        .type('723b4c763114904392ca441909aa0375')
+        .should('have.value', '723b4c763114904392ca441909aa0375');
+
+    cy.get('#testTmdbKey')
+        .click();
+
+    cy.get('#tmdbTestError')
+        .should('not.be.visible');
+
+    cy.get('#tmdbTestSuccess')
+        .should('be.visible');
+
+    cy.get('#tmdbSaveError')
+        .should('not.be.visible');
+
+    cy.get('#tmdbSaveSuccess')
+        .should('not.be.visible');
+});
+
+it('Save invalid TMDB Key', () => {
+    cy.visit('/configuration', {onBeforeLoad: spyOnAddEventListener});
+
+    cy.get('#movieDbApiKey')
+        .clear()
+        .type('ABC123')
+        .should('have.value', 'ABC123');
+
+    cy.get('#saveTmdbKey')
+        .click();
+
+    cy.get('#tmdbTestError')
+        .should('not.be.visible');
+
+    cy.get('#tmdbTestSuccess')
+        .should('not.be.visible');
+
+    cy.get('#tmdbSaveError')
+        .should('not.be.visible');
+
+    cy.get('#tmdbSaveSuccess')
+        .should('be.visible');
+
+    cy.visit('/configuration', {onBeforeLoad: spyOnAddEventListener});
+
+    cy.get('#movieDbApiKey')
+        .should('have.value', 'ABC123');
+});
+
+it('Save valid TMDB Key', () => {
+    cy.visit('/configuration', {onBeforeLoad: spyOnAddEventListener});
+
+    cy.get('#movieDbApiKey')
+        .clear()
+        .type('723b4c763114904392ca441909aa0375')
+        .should('have.value', '723b4c763114904392ca441909aa0375');
+
+    cy.get('#saveTmdbKey')
+        .click();
+
+    cy.get('#tmdbTestError')
+        .should('not.be.visible');
+
+    cy.get('#tmdbTestSuccess')
+        .should('not.be.visible');
+
+    cy.get('#tmdbSaveError')
+        .should('not.be.visible');
+
+    cy.get('#tmdbSaveSuccess')
+        .should('be.visible');
+
+    cy.visit('/configuration', {onBeforeLoad: spyOnAddEventListener});
+
+    cy.get('#movieDbApiKey')
+        .should('have.value', '723b4c763114904392ca441909aa0375');
+});
--- cypress/integration/form.spec.js
+++
@@ -1,5 +0,0 @@
-describe("Index test", () => {
-    it("Can it make it to the index page", () => {
-        cy.visit("/");
-    });
-});
