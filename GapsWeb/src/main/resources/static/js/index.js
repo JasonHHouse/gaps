23fbe3eb0952961b115063987f6b35697151733d
Jason House <jh5975@gmail.com>
2020-08-19 12:12:02 +0900

    0.5.2 - Fixed bug with notifications not sending when set

--- GapsWeb/src/main/resources/static/js/index.js
+++ GapsWeb/src/main/resources/static/js/index.js
@@ -14,9 +14,9 @@
     'use strict';
     window.addEventListener('load', function () {
         // Fetch all the forms we want to apply custom Bootstrap validation styles to
-        var forms = document.getElementsByClassName('needs-validation');
+        const forms = document.getElementsByClassName('needs-validation');
         // Loop over them and prevent submission
-        var validation = Array.prototype.filter.call(forms, function (form) {
+        Array.prototype.filter.call(forms, function (form) {
             form.addEventListener('submit', function (event) {
                 if (form.checkValidity() === false) {
                     event.preventDefault();
