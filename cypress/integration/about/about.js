048b788f6296bbc6706b0170229ff6c2d9e66ecc
Jason House <jh5975@gmail.com>
2020-08-21 13:30:27 +0900

    0.5.4

--- cypress/integration/about/about.js
+++ cypress/integration/about/about.js
@@ -9,7 +9,7 @@ describe('Verify About Page', function () {
             .should('have.text', 'About');
 
         cy.get('.container > :nth-child(3)')
-            .should('have.text', 'v0.5.3');
+            .should('have.text', 'v0.5.4');
 
         cy.get('.container > :nth-child(6)')
             .should('have.text', 'Software');
