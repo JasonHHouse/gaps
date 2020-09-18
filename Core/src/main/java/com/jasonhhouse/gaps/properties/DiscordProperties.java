432880f2acf448841d6a05fd27ebfe070f61f4a6
Jason House <jh5975@gmail.com>
2020-09-18 10:38:43 +0900

    Adding properties classes

---
+++ Core/src/main/java/com/jasonhhouse/gaps/properties/DiscordProperties.java
@@ -0,0 +1,53 @@
+/*
+ * Copyright 2020 Jason H House
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ */
+
+package com.jasonhhouse.gaps.properties;
+
+import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.jasonhhouse.gaps.NotificationType;
+import java.util.Collections;
+import java.util.List;
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
+
+@JsonIgnoreProperties(ignoreUnknown = true)
+@JsonInclude(JsonInclude.Include.NON_NULL)
+public final class DiscordProperties extends AbstractNotificationProperties {
+
+    @NotNull
+    private final String webHookUrl;
+
+    @JsonCreator(mode = JsonCreator.Mode.PROPERTIES)
+    public DiscordProperties(@JsonProperty(value = "enabled", required = true) @NotNull Boolean enabled,
+                             @JsonProperty(value = "notificationTypes", required = true) @NotNull List<NotificationType> notificationTypes,
+                             @JsonProperty(value = "webHookUrl") @Nullable String webHookUrl) {
+        super(enabled, notificationTypes);
+        this.webHookUrl = webHookUrl == null ? "" : webHookUrl;
+    }
+
+    static DiscordProperties getDefault() {
+        return new DiscordProperties(false, Collections.emptyList(), "");
+    }
+
+    @Override
+    public String toString() {
+        return "DiscordProperties{" +
+                "webHookUrl='" + webHookUrl + '\'' +
+                '}';
+    }
+
+    @NotNull
+    public String getWebHookUrl() {
+        return webHookUrl;
+    }
+}
---
+++ GapsWeb/src/main/java/com/jasonhhouse/gaps/notifications/DiscordNotificationAgent.java
@@ -0,0 +1,163 @@
+/*
+ * Copyright 2020 Jason H House
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ */
+
+package com.jasonhhouse.gaps.notifications;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.jasonhhouse.gaps.NotificationType;
+import com.jasonhhouse.gaps.properties.SlackProperties;
+import com.jasonhhouse.gaps.service.FileIoService;
+import java.io.IOException;
+import java.util.concurrent.TimeUnit;
+import okhttp3.Headers;
+import okhttp3.HttpUrl;
+import okhttp3.MediaType;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.RequestBody;
+import okhttp3.Response;
+import org.jetbrains.annotations.Nullable;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import static com.jasonhhouse.gaps.notifications.NotificationStatus.TIMEOUT;
+
+public class DiscordNotificationAgent extends AbstractNotificationAgent<SlackProperties> {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(DiscordNotificationAgent.class);
+    private static final ObjectMapper objectMapper = new ObjectMapper();
+    private final OkHttpClient client;
+
+    public DiscordNotificationAgent(FileIoService fileIoService) {
+        super(fileIoService);
+
+        client = new OkHttpClient.Builder()
+                .connectTimeout(TIMEOUT, TimeUnit.MILLISECONDS)
+                .readTimeout(TIMEOUT, TimeUnit.MILLISECONDS)
+                .writeTimeout(TIMEOUT, TimeUnit.MILLISECONDS)
+                .build();
+    }
+
+    @Override
+    public int getId() {
+        return 2;
+    }
+
+    @Override
+    public String getName() {
+        return "Slack Notification Agent";
+    }
+
+    @Override
+    public boolean sendMessage(NotificationType notificationType, String level, String title, String message) {
+        LOGGER.info("sendMessage( {}, {}, {} )", level, title, message);
+
+        if (sendPrepMessage(notificationType)) {
+            return false;
+        }
+
+        HttpUrl url = HttpUrl.get(t.getWebHookUrl());
+
+        Headers headers = new Headers.Builder()
+                .add("Content-Type", org.springframework.http.MediaType.APPLICATION_JSON_VALUE)
+                .build();
+
+        Slack slack = new Slack(String.format("*%s*%n%s", title, message));
+
+        String slackMessage = "";
+        try {
+            slackMessage = objectMapper.writeValueAsString(slack);
+        } catch (JsonProcessingException e) {
+            LOGGER.error("Failed to turn Slack message into JSON", e);
+            return false;
+        }
+
+        LOGGER.info("slackMessage {}", slackMessage);
+        RequestBody body = RequestBody.create(slackMessage, MediaType.get(org.springframework.http.MediaType.APPLICATION_JSON_VALUE));
+
+        Request request = new Request.Builder()
+                .headers(headers)
+                .url(url)
+                .post(body)
+                .build();
+
+        try (Response response = client.newCall(request).execute()) {
+            if (response.isSuccessful()) {
+                LOGGER.info("Slack message sent via {}", url);
+                return true;
+            } else {
+                LOGGER.error("Error with Slack Url: {} Body returned {}", url, response.body());
+                return false;
+            }
+
+        } catch (IOException e) {
+            LOGGER.error(String.format("Error with Slack Url: %s", url), e);
+            return false;
+        }
+    }
+
+    @Nullable
+    @Override
+    public SlackProperties getNotificationProperties() {
+        return fileIoService.readProperties().getSlackProperties();
+    }
+
+    private static final class Slack {
+        private final Block[] blocks;
+
+        private Slack(String message) {
+            blocks = new Block[1];
+            blocks[0] = new DiscordNotificationAgent.Block(new Text(message));
+        }
+
+        public Block[] getBlocks() {
+            return blocks;
+        }
+    }
+
+    private static final class Block {
+        private final String type;
+        private final Text text;
+
+        private Block(Text text) {
+            this.type = "section";
+            this.text = text;
+        }
+
+        public String getType() {
+            return type;
+        }
+
+        public Text getText() {
+            return text;
+        }
+    }
+
+    private static final class Text {
+        private final String type;
+        private final String value;
+
+        private Text(String value) {
+            this.type = "mrkdwn";
+            this.value = value;
+        }
+
+        public String getType() {
+            return type;
+        }
+
+        @JsonProperty("text")
+        public String getValue() {
+            return value;
+        }
+    }
+}
