1b75b5e6a1c34fd79e6ed15dc917229dc5b2bb93
Jason <jh5975@gmail.com>
2021-03-18 18:24:09 +0900

    ToDo

--- GapsWeb/src/main/resources/static/js/modules/slack-notifications.js
+++ GapsWeb/src/main/resources/static/js/modules/slack-notifications.js
@@ -8,6 +8,12 @@
  *  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
  */
 
+// ToDo
+// Update all the calls with the new common function
+// Test in cypress
+// Update docker
+// Push and notify on github
+
 import getNotificationTypes from './notification-types.min.js';
 import Payload from './payload.min.js';
 import hideAllAlertsAndSpinners from './alerts-manager.min.js';
