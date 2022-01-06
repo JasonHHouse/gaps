6a7ebbb27e21e16bf4d57648eb4d454f3f9b5cd1
Jason House <jh5975@gmail.com>
2022-01-05 20:01:12 -0500

    Fixing start.sh to not show errors

--- start.sh
+++ start.sh
@@ -3,14 +3,14 @@ echo "Enable SSL: $ENABLE_SSL"
 echo "Enable Login: $ENABLE_LOGIN"
 echo "Base URL: $BASE_URL"
 echo "Jar File: $JAR_FILE"
-if [ $ENABLE_SSL == true ]; then
-  if [ $ENABLE_LOGIN == true ]; then
+if [ "$ENABLE_SSL" = true ]; then
+  if [ "$ENABLE_LOGIN" = true ]; then
     java -Xms256m -Xmx2048m -jar -Dspring.profiles.active=ssl -Dserver.servlet.context-path=$BASE_URL $JAR_FILE
   else
     java -Xms256m -Xmx2048m -jar -Dspring.profiles.active=ssl-no-login -Dserver.servlet.context-path=$BASE_URL $JAR_FILE
   fi
 else
-    if [ $ENABLE_LOGIN == true ]; then
+    if [ "$ENABLE_LOGIN" = true ]; then
       java -Xms256m -Xmx2048m -jar -Dspring.profiles.active=no-ssl -Dserver.servlet.context-path=$BASE_URL $JAR_FILE
     else
       java -Xms256m -Xmx2048m -jar -Dspring.profiles.active=no-ssl-no-login -Dserver.servlet.context-path=$BASE_URL $JAR_FILE
